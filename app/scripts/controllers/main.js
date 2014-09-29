e607e35d89906871dc60a1c89f8a6c4f96290373
Shavin Fonseka <shavindra2001@yahoo.co.uk>
2014-09-30 00:17:51 +0100

    Bus App added the code

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name busAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the busAppApp
+ */
+angular.module('busAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
