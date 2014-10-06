9afd27edaeb85efa2f6e8688538a0dc9d7964c9f
Shavin Fonseka <shavin.fonseka@cityindex.com>
2014-10-06 14:26:57 +0100

    Re-generated the app from scratch...
    Redoing this again in order to deploy to Heroku

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name busAppApp
+ * @description
+ * # busAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('busAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
